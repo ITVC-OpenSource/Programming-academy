79f1c34a2a38f0819be8bf034d40637616dc49b9
AMYSoft <info@amy98.ir>
2021-09-21 08:09:53 +0430

    Update code.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 include(__DIR__ . "/assets/php/router.php");
 include(__DIR__ . "/assets/php/config.php");
+ROUTE::get("/login/" , function(){
+  include(__DIR__ . "/assets/pages/login.php");
+});
 ?>
